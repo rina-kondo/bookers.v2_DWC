4c40e4f0c3277fd2281298df4706ffce12b963fe
rina-kondo <122541296+rina-kondo@users.noreply.github.com>
2023-04-09 15:25:23 +0000

    updated

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
+  def index
+    @users = User.all
+    # _bookNewに使用
+    @book = Book.new
+  end
+
+  def show
+    # ユーザーの投稿内容
+    @user = User.find(params[:id])
+    @books = @user.books.all
+    # _bookNewに使用
+    @book = Book.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  root to: "homes#top"
+  get 'about' => 'homes#about', as: 'about'
+  devise_for :users
+
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :edit, :show, :update]
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
